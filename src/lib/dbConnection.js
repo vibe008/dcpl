edfc3900296ccb143bd424f4a23b4fa90a42ff98
Vivek Bunker <vivekbunker65@gmail.com>
2026-01-02 15:33:14 +0530

    complete demo

--- src/lib/dbConnection.js
+++ src/lib/dbConnection.js
@@ -1,24 +1,26 @@
 import mongoose from "mongoose";
 
-const MONGO_URL = process.env.MONGO_URI
-
-if (!MONGO_URL) {
-    throw new Error("Please define the MONGODB_URI in .env")
-}
-
 let cached = global.mongoose;
 
 if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 export async function dbConnect() {
-    if (cached.conn) return cached.conn;
-    if (!cached.promise) {
-        cached.promise = mongoose.connect(MONGO_URL, {
-            bufferCommands: false,
-        }).then((mongoose) => mongoose);
-    }
-    cached.conn = await cached.promise;
-    return cached.conn;
+  if (cached.conn) return cached.conn;
+
+  const MONGO_URI = process.env.MONGO_URI; 
+
+  if (!MONGO_URI) {
+    throw new Error("MONGO_URI is not defined");
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGO_URI, { bufferCommands: false })
+      .then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
